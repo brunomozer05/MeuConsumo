0b248ebeb661897f092dc558b1351a31434cc014
unknown <brunomozer05@gmail.com>
2024-11-13 19:26:53 -0300

    Implementação da validação de inputs vazios e melhorias no frontend

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-function calcular(){
-   let distancia = document.getElementById(`dist`)
-   let consumo = document.getElementById(`cons`)
-   let preco = document.getElementById(`preco`)
+function calcular() {
+   let distancia = Number(document.getElementById(`dist`).value)
+   let consumo = Number(document.getElementById(`cons`).value)
+   let preco = Number(document.getElementById(`preco`).value)
    let res = document.getElementById(`res`)
-   let val = Number(distancia.value / consumo.value * preco.value)
-   let litro = Number(val / preco.value)
-   res.innerHTML = `A viagem vai custar: R$ ${val}`
-   res.innerHTML += `<br>consumo de combustivel : ${litro.toFixed(2)} L`
+
+   if (distancia == 0 || consumo == 0 || preco == 0) {
+      alert('[ERRO] Por gentileza, preencha os dados do formulário corretamente.');
+   } else {
+      let val = (distancia / consumo) * preco
+      let litro = val / preco
+      res.innerHTML = `A viagem vai custar: R$ ${val}`
+      res.innerHTML += `<br>consumo de combustivel : ${litro.toFixed(2)} L`
+   }
 }
